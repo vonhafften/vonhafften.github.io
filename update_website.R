9d937ca5fea1709f46672ccb3be6c2c884b56afc
Alexander von Hafften <alexanderhvonhafften@gmail.com>
2021-07-07 10:11:18 -0500

    homepage redo

--- update_website.R
+++ update_website.R
@@ -1,3 +1,4 @@
-setwd("Documents/vonhafften.github.io/")
+setwd("/Documents/vonhafften.github.io/")
 
+rmarkdown::render('index.Rmd', output_format = "html_document")
 rmarkdown::render('econ714b_pastexamquestions.Rmd', output_format = c("html_document", "pdf_document"))
