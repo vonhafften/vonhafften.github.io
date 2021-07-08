e8d192909ef710f98c33f360af6b80a19cba2548
Alexander von Hafften <alexanderhvonhafften@gmail.com>
2021-07-07 22:06:43 -0500

    shiny app added

--- update_website.R
+++ update_website.R
@@ -1,7 +1,9 @@
 setwd("/Documents/vonhafften.github.io/")
 
-rmarkdown::render('index.Rmd', output_format = "html_document")
-rmarkdown::render('research.Rmd', output_format = "html_document")
-rmarkdown::render('teaching.Rmd', output_format = "html_document")
+library(rmarkdown)
 
-rmarkdown::render('econ714b_pastexamquestions.Rmd', output_format = c("html_document", "pdf_document"))
+render('index.Rmd', output_format = "html_document")
+render('research.Rmd', output_format = "html_document")
+render('teaching.Rmd', output_format = "html_document")
+
+render('econ714b_pastexamquestions.Rmd', output_format = c("html_document", "pdf_document"))
