02bf56e815a32591acb2f3e541eceedd92a8cec1
vonhafften <alexanderhvonhafften@gmail.com>
2022-05-03 08:56:30 -0500

    link fixed

--- update_website.R
+++ update_website.R
@@ -1,4 +1,4 @@
-setwd("/Users/alexandervonhafften/Documents/vonhafften.github.io/")
+setwd("/Users/vonhafften/Documents/vonhafften.github.io/")
 
 library(rmarkdown)
 
@@ -6,4 +6,4 @@ render('index.Rmd', output_format = "html_document")
 render('research.Rmd', output_format = "html_document")
 render('teaching.Rmd', output_format = "html_document")
 
-render('econ714b_pastexamquestions.Rmd', output_format = c("html_document", "pdf_document"))
+render('teaching/econ714b_pastexamquestions.Rmd', output_format = c("html_document", "pdf_document"))
