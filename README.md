a6b7524c194eced0a741d6ed3c5cdde14f52ef9d
vonhafften <alexanderhvonhafften@gmail.com>
2023-10-29 16:21:27 -0500

    fixed

---
+++ README.md
@@ -0,0 +1,1 @@
+# vonhafften.github.io
---
+++ update_website.R
@@ -0,0 +1,9 @@
+setwd("/Users/vonhafften/Documents/vonhafften.github.io/")
+
+library(rmarkdown)
+
+render('index.Rmd', output_format = "html_document")
+render('research.Rmd', output_format = "html_document")
+render('teaching.Rmd', output_format = "html_document")
+
+render('teaching/econ714b_pastexamquestions.Rmd', output_format = c("html_document", "pdf_document"))
