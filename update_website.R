aaf30b79feb7f91163c68fb4c03f5f7650db9273
vonhafften <alexanderhvonhafften@gmail.com>
2023-10-29 16:20:14 -0500

    Paper with Michael added

--- update_website.R
+++
@@ -1,9 +0,0 @@
-setwd("/Users/vonhafften/Documents/vonhafften.github.io/")
-
-library(rmarkdown)
-
-render('index.Rmd', output_format = "html_document")
-render('research.Rmd', output_format = "html_document")
-render('teaching.Rmd', output_format = "html_document")
-
-render('teaching/econ714b_pastexamquestions.Rmd', output_format = c("html_document", "pdf_document"))
